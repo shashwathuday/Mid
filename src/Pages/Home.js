a69d1c2d1afdea52f5dd1ac67d1ac45e6c533292
Shashwath Udaya Kumar <46096060+shashwathuday@users.noreply.github.com>
2024-02-29 12:46:42 -0500

    Add project to git

---
+++ src/Pages/Home.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <div className="card mb-3" styles="max-width: 540px;">
+            <div className="row g-0">
+                <div className="col-md-4">
+                {/* <img src="..." class="img-fluid rounded-start" alt="..."> */}
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Placeholder.</p>
+                        <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="card mb-3" styles="max-width: 540px;">
+            <div className="row g-0">
+                <div className="col-md-4">
+                {/* <img src="..." class="img-fluid rounded-start" alt="..."> */}
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Placeholder.</p>
+                        <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="card mb-3" styles="max-width: 540px;">
+            <div className="row g-0">
+                <div className="col-md-4">
+                {/* <img src="..." class="img-fluid rounded-start" alt="..."> */}
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Placeholder.</p>
+                        <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
